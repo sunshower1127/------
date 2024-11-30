sunshower1127/Lecture-Automation-with-Selenium

4.0버전: selenium_wrapper_3로 만듦

main.py
@@ -1,78 +1,90 @@
-"""숭실대학교 LMS 동영상 강의를 자동으로 시청하는 프로그램입니다."""
+import os
+from time import sleep
 
-from sw_selenium.driver import SwChrome
-from sw_selenium.ui_manager import get_data_from_file_or_ui
-
-id, pw = get_data_from_file_or_ui(  # noqa: A001
-    file_path="숭실대계정정보.txt",
-    prompt_message="숭실대 아이디와 비밀번호를 입력해주세요.",
-    length=2,
+from dotenv import load_dotenv
+from selenium_wrapper_3.builder import ChromeBuilder
+from selenium_wrapper_3.driver import Driver, driver
+from selenium_wrapper_3.kor_date_parser import convert_date
+from selenium_wrapper_3.node import (
+    A,
+    Any,
+    Button,
+    Div,
+    I,
+    IFrame,
+    Input,
+    Parent,
+    Span,
+)
+from selenium_wrapper_3.utils import (
+    click,
+    count,
+    frame,
+    no_error,
+    populate,
+    retry_until,
+    send_keys,
+    text,
+    url,
 )
 
-ban_list = get_data_from_file_or_ui(
-    file_path="금지할강의명.txt",
-    prompt_message="금지할 강의명을 입력해주세요.\n(여러개 입력가능, 더이상 입력하지 않으려면 취소)",
-    length="INF",
-)
+load_dotenv()
 
-web = SwChrome(
-    "disable_popup", "mute_audio", "prevent_sleep", "maximize", "headless", timeout=20
-)
+ChromeBuilder().debug_setting().set_retry(0.3, 5).add_option("maximize").build()
 
-web.get("https://lms.ssu.ac.kr/login")
-web.find(name="userid").send_keys(id, "\t", pw, "\n")
-web.find(text="마이페이지").up.click()
+url("https://lms.ssu.ac.kr/login")
 
-web.goto_frame("/fulliframe")
+id, pw = os.getenv("ID"), os.getenv("PW")  # noqa: A001
+assert id and pw, "ID and PW must be set"  # noqa: PT018, S101
 
-while True:
-    # 볼 영상이 있는 과목 찾기
-    subject_btns = (
-        web.find_all(tag="span", class_name="xntc-title", text="동영상")
-        .left()
-        .wait(1)
-        .filter(text="!0")
+send_keys(Input(name="userid"), [id, "\t", pw, "\n"])
+
+click(Any(text="마이페이지") / Parent)
+
+with frame(IFrame(id="fulliframe")):
+    lines = Div(class_="xnscc-header")
+
+    enable_subjects = retry_until(
+        lambda: [line for line in populate(lines) if text(line // A()) != "0"],
     )
 
-    selected = False
+for subject in enable_subjects:
+    url("https://lms.ssu.ac.kr/mypage")
+    with frame(IFrame(id="fulliframe")):
+        click(subject // Button())
 
-    # 과목의 봐야할 동영상 강의 찾기
-    for subject_btn in subject_btns:
-        subject_btn.find_all(axis="preceding", tag="button")[-1].click()
-        titles = web.find_all(tag="i", class_name="xnsti-left-icon video").right(0).text
-        titles = [f"'{title}'" for title in titles if title not in ban_list]
-        print(*titles, sep=", ")
+        courses = subject / Parent // Div(class_="xnsti-left")
 
-        if titles:
-            selected = True
-            break
-        ##
+        titles = retry_until(
+            lambda courses=courses: [
+                text(course / A())
+                for course in populate(courses)
+                if count(course / I(("video", "in", "class")))
+            ]
+        )
 
-    if not selected:
-        break
+        click(subject // A())
 
-    subject_btn.click()
-
-    # 강의 보기
     for title in titles:
-        web.goto_frame("/tool_content")
-        web.find(tag="a", text=title).click()
+        with frame(IFrame(id="tool_content")):
+            click(A(text=title))
 
-        web.goto_frame("/tool_content")
-        total_dur = web.find(text="재생 시간").right[0].text
+        with frame(IFrame(id="tool_content")):
+            total_dur = text(Span(text="재생 시간") / Parent / Span(1))
 
-        cur_dur = web.find(text="학습 진행 상태").right[0].text.split("(")[0]
-        web.goto_frame("0")
-        web.find(title="재생").click()  # maximize 안해서 에러였었나 모르겠네
+            cur_dur = text(Span(text="학습 진행 상태") / Parent / Span(1)).split("(")[0]
 
-        with web.no_exc(), web.set_retry(10):
-            web.find(text="예").click()
-            web.find(text="확인").click()
+            with frame(IFrame()):
+                click(Div(title="재생"))
+
+                with no_error(), Driver().set_retry(1, 10):
+                    click(Any(text="예"))
+                    click(Any(text="확인"))
 
         print("Watching", title)
-        web.wait((cur_dur, total_dur), display=True)
-        web.back()
 
-    web.goto_frame("/")
-    web.find(text_contains="마이페이지").up.click()
-    web.goto_frame("tool_content")
+        t = convert_date(total_dur) - convert_date(cur_dur)
+        print(t)
+        sleep(t.total_seconds())
+
+        driver().back()

sunshower1127/------

main.py
@@ -1,78 +1,70 @@
-"""숭실대학교 LMS 동영상 강의를 자동으로 시청하는 프로그램입니다."""
+import os
+from time import sleep
 
-from sw_selenium.driver import SwChrome
-from sw_selenium.ui_manager import get_data_from_file_or_ui
+from dotenv import load_dotenv
+from selenium_wrapper_3.addons.kor_date_parser import convert_date
+from selenium_wrapper_3.builder import ChromeBuilder
+from selenium_wrapper_3.driver import Driver
+from selenium_wrapper_3.node import *  # type: ignore[attr-defined]
+from selenium_wrapper_3.util import *  # type: ignore[attr-defined]
 
-id, pw = get_data_from_file_or_ui(  # noqa: A001
-    file_path="숭실대계정정보.txt",
-    prompt_message="숭실대 아이디와 비밀번호를 입력해주세요.",
-    length=2,
-)
+load_dotenv()
 
-ban_list = get_data_from_file_or_ui(
-    file_path="금지할강의명.txt",
-    prompt_message="금지할 강의명을 입력해주세요.\n(여러개 입력가능, 더이상 입력하지 않으려면 취소)",
-    length="INF",
-)
+ChromeBuilder().debug_setting().set_retry(0.3, 5).add_option("maximize").build()
 
-web = SwChrome(
-    "disable_popup", "mute_audio", "prevent_sleep", "maximize", "headless", timeout=20
-)
+url("https://lms.ssu.ac.kr/login")
 
-web.get("https://lms.ssu.ac.kr/login")
-web.find(name="userid").send_keys(id, "\t", pw, "\n")
-web.find(text="마이페이지").up.click()
+id, pw = os.getenv("ID"), os.getenv("PW")  # noqa: A001
+assert id and pw, "ID and PW must be set"  # noqa: PT018, S101
 
-web.goto_frame("/fulliframe")
+send_keys(Input(name="userid"), [id, "\t", pw, "\n"])
 
-while True:
-    # 볼 영상이 있는 과목 찾기
-    subject_btns = (
-        web.find_all(tag="span", class_name="xntc-title", text="동영상")
-        .left()
-        .wait(1)
-        .filter(text="!0")
+click(Any(text="마이페이지") / __)
+
+with frame(IFrame(id="fulliframe")):
+    lines = Div(class_="xnscc-header")
+
+    enable_subjects = retry_until(
+        lambda: [line for line in populate(lines) if text(line // A()) != "0"],
     )
 
-    selected = False
+for subject in enable_subjects:
+    url("https://lms.ssu.ac.kr/mypage")
+    with frame(IFrame(id="fulliframe")):
+        click(subject // Button())
 
-    # 과목의 봐야할 동영상 강의 찾기
-    for subject_btn in subject_btns:
-        subject_btn.find_all(axis="preceding", tag="button")[-1].click()
-        titles = web.find_all(tag="i", class_name="xnsti-left-icon video").right(0).text
-        titles = [f"'{title}'" for title in titles if title not in ban_list]
-        print(*titles, sep=", ")
+        courses = subject / __ // Div(class_="xnsti-left")
 
-        if titles:
-            selected = True
-            break
-        ##
+        titles = retry_until(
+            lambda courses=courses: [
+                text(course / A())
+                for course in populate(courses)
+                if count(course / I(("video", "in", "class")))
+            ]
+        )
 
-    if not selected:
-        break
+        click(subject // A())
 
-    subject_btn.click()
-
-    # 강의 보기
     for title in titles:
-        web.goto_frame("/tool_content")
-        web.find(tag="a", text=title).click()
+        with frame(IFrame(id="tool_content")):
+            click(A(text=title))
 
-        web.goto_frame("/tool_content")
-        total_dur = web.find(text="재생 시간").right[0].text
+        with frame(IFrame(id="tool_content")):
+            total_dur = text(Span(text="재생 시간") / __ / Span(1))
 
-        cur_dur = web.find(text="학습 진행 상태").right[0].text.split("(")[0]
-        web.goto_frame("0")
-        web.find(title="재생").click()  # maximize 안해서 에러였었나 모르겠네
+            cur_dur = text(Span(text="학습 진행 상태") / __ / Span(1)).split("(")[0]
 
-        with web.no_exc(), web.set_retry(10):
-            web.find(text="예").click()
-            web.find(text="확인").click()
+            with frame(IFrame()):
+                click(Div(title="재생"))
+
+                with no_error(), Driver().set_retry(1, 10):
+                    click(Any(text="예"))
+                    click(Any(text="확인"))
 
         print("Watching", title)
-        web.wait((cur_dur, total_dur), display=True)
-        web.back()
 
-    web.goto_frame("/")
-    web.find(text_contains="마이페이지").up.click()
-    web.goto_frame("tool_content")
+        t = convert_date(total_dur) - convert_date(cur_dur)
+        print(t)
+        sleep(t.total_seconds())
+
+        Driver().web.back()